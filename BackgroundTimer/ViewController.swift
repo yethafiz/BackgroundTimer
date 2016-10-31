0844c6b8730b9c8678252d5f629d7d83af2d09d6
h44f33z <h44f33z@gmail.com>
2016-10-31 15:12:01 +0800

    update code to swift 3

--- BackgroundTimer/ViewController.swift
+++ BackgroundTimer/ViewController.swift
@@ -11,24 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     var backgroundTaskIdentifier: UIBackgroundTaskIdentifier?
-    var timer: NSTimer?
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        backgroundTaskIdentifier = UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler({
-            UIApplication.sharedApplication().endBackgroundTask(self.backgroundTaskIdentifier!)
+        backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+            UIApplication.shared.endBackgroundTask(self.backgroundTaskIdentifier!)
         })
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: "awesomeTask", userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(ViewController.awesomeTask), userInfo: nil, repeats: true)
     }
     
     func awesomeTask() {
-        let date = NSDate()
+        let date = Date()
         print("update on \(date)")
     }
 
-    @IBAction func stopTImerHandler(sender: UIButton) {
+    @IBAction func stopTImerHandler(_ sender: UIButton) {
         timer?.invalidate()
     }
     override func didReceiveMemoryWarning() {
